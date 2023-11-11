c8f7df4d7adcab6ebe1f7acfecb278671f2a365e
nolimitsexpo <143121681+nolimitsexpo@users.noreply.github.com>
2023-11-11 21:53:48 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,14 @@ function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
       <DefaultSeo
-        title="Download Thumbnail From Youtube"
-        description="Download high-quality thumbnails from YouTube videos."
-        canonical="https://your-website-url.com"
+        title="YouTube Thumbnail Downloader - Download Thumbnails from Youtube in Different Qualities"
+        description="Download thumbnails from YouTube videos in different qualities using this simple YouTube Thumbnail Downloader"
+        canonical="https://www.downloadthumbnailfromyt.com/"
         openGraph={{
-          url: "https://your-website-url.com",
-          title: "Downloade Thumbnail From Youtube",
+          url: "https://www.downloadthumbnailfromyt.com/",
+          title: "YouTube Thumbnail Downloader - Download Thumbnails from Youtube in Different Qualities",
           description: "Download high-quality thumbnails from YouTube videos. with Our Youtube Thumbnail Grabber",
-          site_name: "Download Thumbnail From Youtube",
+          site_name: "Download Thumbnail From Youtube in Different Qualities",
         }}
       />
       <Component {...pageProps} />
