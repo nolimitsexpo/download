534a3e1707bfef8bb573a6ed7d7c16d7ce4b689e
nolimitsexpo <143121681+nolimitsexpo@users.noreply.github.com>
2023-11-11 20:34:14 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ const Index = () => {
     <div className="container mx-auto px-4 py-8">
       <header className="text-center mb-8">
         <h1 className="text-3xl font-bold mb-2">
-          Youtube Thumbnail Downloader
+          Download Thumbnail From Youtube
         </h1>
         <p className="text-gray-600">
           Download high-quality thumbnails from YouTube videos.
