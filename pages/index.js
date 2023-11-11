adc6bc5f8fa132327fec23c424b89826f490294e
nolimitsexpo <143121681+nolimitsexpo@users.noreply.github.com>
2023-11-11 21:17:48 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,21 +5,18 @@ const Index = () => {
   const [videoURL, setVideoURL] = useState("");
   const [thumbnailOptions, setThumbnailOptions] = useState([]);
 
+  const downloadThumbnail = (url, resolution) => {
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = `thumbnail_${resolution.replace(/\s+/g, "_")}.jpg`;
+    link.click();
+  };
+
   const getYouTubeThumbnail = (url) => {
-    let regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
-    let match = url.match(regExp);
+    // ... (your existing code)
 
     if (match && match[1].length === 11) {
-      const videoURL = match[1];
-      const thumbnailBaseUrl = "http://img.youtube.com/vi/";
-
-      const options = [
-        { resolution: "HD (1280x720)", code: "maxresdefault" },
-        { resolution: "SD (640x480)", code: "sddefault" },
-        { resolution: "Normal (480x360)", code: "hqdefault" },
-        { resolution: "Medium (320x180)", code: "mqdefault" },
-        { resolution: "Low (120x90)", code: "default" },
-      ];
+      // ... (your existing code)
 
       const thumbnailOptions = options.map((option) => ({
         resolution: option.resolution,
@@ -35,29 +32,7 @@ const Index = () => {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <header className="text-center mb-8">
-        <h1 className="text-3xl font-bold mb-2">
-          Download Thumbnail From Youtube
-        </h1>
-        <p className="text-gray-600">
-          Download high-quality thumbnails from YouTube videos.
-        </p>
-      </header>
-      <div className="text-center">
-        <input
-          type="text"
-          className="w-full md:w-1/2 px-4 py-2 border rounded"
-          placeholder="Enter YouTube URL"
-          value={videoURL}
-          onChange={(e) => setVideoURL(e.target.value)}
-        />
-        <button
-          className="btn-blue mt-2"
-          onClick={() => getYouTubeThumbnail(videoURL)}
-        >
-          Download Thumbnails
-        </button>
-      </div>
+      {/* ... (your existing code) */}
       {thumbnailOptions.length > 0 && (
         <div className="mt-8">
           <h2 className="text-xl font-semibold mb-4">Thumbnail Options</h2>
@@ -67,9 +42,12 @@ const Index = () => {
                 <img src={option.url} alt={`Thumbnail ${index + 1}`} />
                 <button
                   className="btn-blue mt-2"
-                  onClick={() => copy(option.url)}
+                  onClick={() => {
+                    copy(option.url);
+                    downloadThumbnail(option.url, option.resolution);
+                  }}
                 >
-                  Copy Image URL
+                  Copy & Download
                 </button>
               </div>
             ))}
