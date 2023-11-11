516530c8744c4ae26b103fca193d2ef469ff1590
nolimitsexpo <143121681+nolimitsexpo@users.noreply.github.com>
2023-11-11 20:22:02 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -23,14 +23,6 @@ class MyDocument extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-364TCL2H0V"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-364TCL2H0V');
-</script>
         </Head>
         <body>
           <Main />
