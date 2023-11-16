2a90dd98686a2d443ba7dfd1d76aceaf004ed170
nolimitsexpo <143121681+nolimitsexpo@users.noreply.github.com>
2023-11-16 16:11:01 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,90 +1,87 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
-import { Analytics } from '@vercel/analytics/react';
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
-        <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/favicon/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/favicon/site.webmanifest" />
+    <Html lang="en">
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
 
-          {/* Add your Google Analytics script */}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-364TCL2H0V"></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-364TCL2H0V');
-              `,
-            }}
-          />
+        {/* Add your Google Analytics script */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-364TCL2H0V"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-364TCL2H0V');
+            `,
+          }}
+        />
 
-          {/* Add styles for the menu */}
-          <style>{`
-            header {
-              text-align: center;
-            }
+        {/* Add styles for the menu */}
+        <style>{`
+          header {
+            text-align: center;
+          }
 
-            nav {
-              display: inline-block;
-            }
+          nav {
+            display: inline-block;
+          }
 
-            ul {
-              list-style: none;
-              padding: 0;
-              margin: 0;
-            }
+          ul {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+          }
 
-            li {
-              display: inline;
-              margin-right: 20px;
-            }
+          li {
+            display: inline;
+            margin-right: 20px;
+          }
 
-            a {
-              color: blue;
-              font-weight: bold;
-              text-decoration: none;
-            }
-          `}</style>
-        </Head>
+          a {
+            color: blue;
+            font-weight: bold;
+            text-decoration: none;
+          }
+        `}</style>
+      </Head>
         
-        <body>
-          <Analytics />
-          
-          {/* Add your menu here */}
-          <header>
-            <nav>
-              <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/privacy-policy">Privacy Policy</a></li>
-                <li><a href="/contact-us">Contact Us</a></li>
-              </ul>
-            </nav>
-          </header>
-          
-          <Main />
-          <NextScript />
-            
+      <body>
+        {/* Add your menu here */}
+        <header>
+          <nav>
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/privacy-policy">Privacy Policy</a></li>
+              <li><a href="/contact-us">Contact Us</a></li>
+            </ul>
+          </nav>
+        
+        </header>
+        
+        <Main />
+        <NextScript />
           <center>
           {/* Add the provided text */}
         <div>
@@ -151,91 +148,6 @@ class MyDocument extends Document {
             using Photoshop or any other software that can make your thumbnail
             unique again so that it can be reused and make it SEO friendly again.
           </p>
-              <p>
-مکمل ایچ ڈی (1080) میں تمام YouTube ویڈیوز کی مفت تھمب نیل تصاویر حاصل کریں۔
-  HD (720)، SD اور یہاں تک کہ چھوٹے سائز۔
-</p>
-<h2>یہ یوٹیوب تھمب نیل گریبر ویب سائٹ کس کے لیے ہے؟ </h2>
-           <p>
-             لوگ کسی بھی یوٹیوب کے تھمب نیلز حاصل کرنے کے لیے اس یوٹیوب تھمب نیل ڈاؤن لوڈ ویب سائٹ کا استعمال کرتے ہیں۔
-             ویڈیو اس کا استعمال پریزنٹیشنز، اینیمیشن ورک، یا بہت سی چیزوں میں کیا جا سکتا ہے۔
-             دیگر مشاغل.
-           </p>
-             <h2>یوٹیوب کے تھمب نیلز کو ڈاؤن لوڈ کرنے کے لیے اس ویب سائٹ کو کیسے استعمال کیا جائے؟ </h2>
-           <p>
-             میں نے کچھ اسکرین شاٹس شیئر کیے ہیں جن میں میں نے آپ کو استعمال کرنے کا طریقہ بتایا ہے۔
-             یہ یوٹیوب ڈاؤنلوڈر ویب سائٹ۔ پہلا قدم بہت آسان ہے:
-             بس کسی بھی یوٹیوب لنک کو کاپی کریں جس کے لیے آپ تھمب نیل چاہتے ہیں۔ ڈاؤن لوڈ کریں
-             یوٹیوب کور آپ آسانی سے اپنے ٹیوب کا کور ڈاؤن لوڈ کرسکتے ہیں۔
-               </p>
-               <p>
-Obtenga imágenes en miniatura gratuitas de todos los vídeos de YouTube en Full HD (1080),
-  HD (720), SD e incluso tamaños pequeños.
-</p>
-<h2>¿Para qué sirve este sitio web YouTube Thumbnail Grabber? </h2>
-           <p>
-             La gente usa este sitio web de descarga de miniaturas de YouTube para obtener miniaturas de cualquier YouTube.
-             video. Esto se puede utilizar en presentaciones, trabajos de animación o muchos
-             otras actividades.
-           </p>
-             <h2>¿Cómo utilizar este sitio web para descargar miniaturas de YouTube? </h2>
-           <p>
-             He compartido algunas capturas de pantalla en las que os he explicado cómo utilizar
-             este sitio web de descarga de YouTube. El primer paso es bastante simple:
-             simplemente copie cualquier enlace de YouTube del que desee una miniatura. descargar
-             Portada de YouTube Puedes descargar fácilmente la portada de tu tubo
-               </p>
-               <p>
-Nhận hình ảnh thu nhỏ miễn phí của tất cả video YouTube ở chế độ Full HD (1080),
-  HD (720), SD và thậm chí cả kích thước nhỏ.
-</p>
-<h2>Trang web YouTube Thumbnail Grabber này dùng để làm gì? </h2>
-           <p>
-             Mọi người sử dụng trang web tải xuống hình thu nhỏ YouTube này để lấy hình thu nhỏ của bất kỳ YouTube nào
-             băng hình. Điều này có thể được sử dụng trong các bài thuyết trình, tác phẩm hoạt hình, hoặc nhiều
-             các hoạt động khác.
-           </p>
-             <h2>Làm cách nào để sử dụng trang web này để tải xuống hình thu nhỏ của YouTube? </h2>
-           <p>
-             Tôi đã chia sẻ một số ảnh chụp màn hình trong đó tôi đã giải thích cho bạn cách sử dụng
-             trang web tải xuống YouTube này. Bước đầu tiên khá đơn giản:
-             chỉ cần sao chép bất kỳ liên kết YouTube nào mà bạn muốn có hình thu nhỏ. Tải xuống
-             Ảnh bìa YouTube Bạn có thể dễ dàng tải xuống bìa ống của mình
-</p>
-               <p>
-Dapatkan gambar mini gratis dari semua video YouTube dalam Full HD (1080),
-  HD (720), SD dan bahkan ukuran kecil.
-</p>
-<h2>Untuk apa situs web Pengambil Gambar Mini YouTube ini? </h2>
-           <p>
-             Orang-orang menggunakan situs pengunduhan thumbnail YouTube ini untuk mendapatkan thumbnail dari YouTube mana pun
-             video. Ini dapat digunakan dalam presentasi, karya animasi, atau banyak lagi
-             Aktifitas lain.
-           </p>
-             <h2>Bagaimana cara menggunakan situs web ini untuk mengunduh thumbnail YouTube? </h2>
-           <p>
-             Saya telah membagikan beberapa tangkapan layar yang telah saya jelaskan kepada Anda cara menggunakannya
-             situs web pengunduh YouTube ini. Langkah pertama cukup sederhana:
-             cukup salin tautan YouTube apa pun yang ingin Anda jadikan thumbnail. unduh
-             Sampul YouTube Anda dapat dengan mudah mengunduh sampul tabung Anda
-</p>
-<p>
-पूर्ण HD (1080) में सभी YouTube वीडियो की निःशुल्क थंबनेल छवियां प्राप्त करें,
-  एचडी (720), एसडी और यहां तक कि छोटे आकार भी।
-</p>
-<h2>यह YouTube थंबनेल ग्रैबर वेबसाइट किस लिए है? </h2>
-           <p>
-             लोग किसी भी YouTube के थंबनेल प्राप्त करने के लिए इस YouTube थंबनेल डाउनलोड वेबसाइट का उपयोग करते हैं
-             वीडियो। इसका उपयोग प्रस्तुतियों, एनीमेशन कार्यों या कई में किया जा सकता है
-             अन्य गतिविधियों।
-           </p>
-             <h2>YouTube थंबनेल डाउनलोड करने के लिए इस वेबसाइट का उपयोग कैसे करें? </h2>
-           <p>
-             मैंने कुछ स्क्रीनशॉट शेयर किये हैं जिसमें मैंने आपको समझाया है कि कैसे इस्तेमाल करना है
-             यह यूट्यूब डाउनलोडर वेबसाइट। पहला चरण बहुत सरल है:
-             बस किसी भी YouTube लिंक को कॉपी करें जिसके लिए आप थंबनेल चाहते हैं। डाउनलोड करना
-             यूट्यूब कवर आप अपने ट्यूब का कवर आसानी से डाउनलोड कर सकते हैं
-</p>
           <p>
             download the thumbnail of a YouTube video
             www.downloadthumbnailfromyt.com - Copyright 2021
